eac4a28a01d82a178b13a52a18aa3a892caa2dfd
Yifan Wang <qcutexu@gmail.com>
2024-04-09 11:18:54 -0500

    Delete mesh directory

--- mesh/convert2hdf5.py
+++
@@ -1,8 +0,0 @@
-from dolfin import *
-mesh = Mesh("pancreas.xml");
-cd=MeshFunction('size_t',mesh,"pancreas_physical_region.xml");
-fd=MeshFunction('size_t',mesh,"pancreas_facet_region.xml");
-hdf = HDF5File(mesh.mpi_comm(), "pancreas.h5", "w")
-hdf.write(mesh, "/mesh")
-hdf.write(cd, "/cd")
-hdf.write(fd, "/fd")
